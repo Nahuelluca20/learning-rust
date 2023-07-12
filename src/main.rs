6747ed80d7c396dd3a0112eeef2912c394e54b48
Nahuel Luca <nahuelluca20@gmail.com>
2023-07-12 19:14:54 -0300

    learn: variables to string

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 fn main() {
-    let age: u8 = 20;
-    // operador "&" se utiliza para tomar una referencia a 
-    // un valor en lugar de poseerlo directamente. Cuando se utiliza "&str", 
-    // estás creando una referencia a una cadena de texto (str) existente en 
-    // lugar de tomar posesión de ella.
-    let name: &str = "Luis Alberto";
-    println!("Hola soy {} y tengo {} años", name, age);
+    // Obtener el nombre
+    println!("Enter your name:");
+    let mut name: String = String::new();
 
+    // std: es la libreria estandar de rust que se contecta con el sistema operativo
+    // io: es la libreria de entrada y salida de datos
+    // stdin: le dice a la maquina que se debe de pedir datos al usuario
+    // read_line: lee una linea de texto
+    // &mut name: esto indica que los datos se guardan en name
+    // unwrap: es un método que se utiliza para desempaquetar el valor 
+    // contenido dentro de un Option o Result.
+    std::io::stdin().read_line(&mut name).unwrap();
+    name = name.trim().to_string();
 
-    // Al parecer las variables tienen que ser escritas en snake_case
-    let min_temperature: i8 = -10;
-    let max_temperature: u8 = 40;
+    // Obtener la edad y convertirlo a string
+    println!("Enter your age:");
+    let mut age: String = String::new();
+    std::io::stdin().read_line(&mut age).unwrap();
+    let age_int: u8 = age.trim().parse().unwrap();
 
-    println!("La temperatura mínima es {} y la máxima es {}", min_temperature, max_temperature)
+    println!("Hello, Welcome {} of {} years old", name, age_int);
 }
 
 
---
+++ src/variablesToString.rs
@@ -0,0 +1,25 @@
+fn variablesToString() {
+  // Obtener el nombre
+  println!("Enter your name:");
+  let mut name: String = String::new();
+
+  // std: es la libreria estandar de rust que se contecta con el sistema operativo
+  // io: es la libreria de entrada y salida de datos
+  // stdin: le dice a la maquina que se debe de pedir datos al usuario
+  // read_line: lee una linea de texto
+  // &mut name: esto indica que los datos se guardan en name
+  // unwrap: es un método que se utiliza para desempaquetar el valor 
+  // contenido dentro de un Option o Result.
+  std::io::stdin().read_line(&mut name).unwrap();
+  name = name.trim().to_string();
+
+  // Obtener la edad y convertirlo a string
+  println!("Enter your age:");
+  let mut age: String = String::new();
+  std::io::stdin().read_line(&mut age).unwrap();
+  let age_int: u8 = age.trim().parse().unwrap();
+
+  println!("Hello, Welcome {} of {} years old", name, age_int);
+}
+
+
